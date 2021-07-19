a09229f3063ab31be7dea7ccf2445f0058c3838b
Aguibou <barrybagata96@gmail.com>
2021-07-19 17:16:27 +0200

    maquette pour les bouton de connexion gmail, facebook et google terminée

---
+++ lib/social_page.dart
@@ -0,0 +1,164 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:yoga/delayed_animation.dart';
+import 'package:yoga/main.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class SocialPage extends StatelessWidget {
+  // const SocialPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // title: Text("social page"),
+        elevation: 0,
+        backgroundColor: Colors.white.withOpacity(0),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          iconSize: 30,
+          color: Colors.black,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+
+        // backgroundColor: Color(d_red),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            DelayedAnimation(
+                child: Container(
+                  height: 200,
+                  child: Image.asset("images/yoga2.jpg"),
+                ),
+                delay: 300),
+            DelayedAnimation(
+                child: Container(
+                  margin:
+                      const EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Change steck here",
+                        style: GoogleFonts.poppins(
+                          color: d_red,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Save your progress to access your personal training ",
+                        style: GoogleFonts.poppins(
+                          color: Colors.grey,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      DelayedAnimation(
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 14,
+                            vertical: 40,
+                          ),
+                          child: Column(
+                            children: [
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: d_red,
+                                  shape: StadiumBorder(),
+                                  padding: const EdgeInsets.all(16),
+                                ),
+                                onPressed: () {},
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.email_outlined,
+                                      size: 30,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      "GMAIL",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.blue,
+                                  shape: StadiumBorder(),
+                                  padding: const EdgeInsets.all(16),
+                                ),
+                                onPressed: () {},
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    FaIcon(FontAwesomeIcons.facebook),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      "FACEBOOK",
+                                      style: TextStyle(fontSize: 20),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.white,
+                                  shape: StadiumBorder(),
+                                  padding: const EdgeInsets.all(16),
+                                ),
+                                onPressed: () {},
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      "images/google.png",
+                                      height: 30,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      "GOOGLE",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        delay: 1500,
+                      )
+                    ],
+                  ),
+                ),
+                delay: 300),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/wellcom_page.dart
+++ lib/wellcom_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:yoga/delayed_animation.dart';
+import 'package:yoga/main.dart';
+import 'package:yoga/social_page.dart';
 
 class WellcomPage extends StatelessWidget {
   @override
@@ -21,16 +23,14 @@ class WellcomPage extends StatelessWidget {
               DelayedAnimation(
                   delay: 2000,
                   child: Container(
-                    height: 300,
+                    height: 200,
                     // color: Colors.blueAccent,
-                    child: Image.asset("images/yoga1.png"),
+                    child: Image.asset("images/yoga12.png"),
                   )),
               DelayedAnimation(
                   delay: 2600,
                   child: Container(
-                      margin: EdgeInsets.only(top: 30),
-                      // height: ,
-                      // color: Colors.blueAccent,
+                      margin: EdgeInsets.only(top: 30, bottom: 20),
                       child: Text(
                           "Debutez vos journées avec pleines d'energie Cours intense de yoga ",
                           textAlign: TextAlign.center,
@@ -38,6 +38,28 @@ class WellcomPage extends StatelessWidget {
                               color: Colors.grey,
                               fontSize: 20,
                               fontStyle: FontStyle.italic)))),
+              DelayedAnimation(
+                  delay: 3600,
+                  child: Container(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: d_red,
+                              shape: StadiumBorder(),
+                              padding: EdgeInsets.all(20)),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SocialPage(),
+                              ),
+                            );
+                          },
+                          child: Text("Get Started",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17,
+                                  fontStyle: FontStyle.italic))))),
             ],
           ),
         ),
